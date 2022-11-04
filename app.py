2b6cc0747ccbd9a174ca3c329eb3df2243f68044
Egor Grishechko <egorgrishechko@gmail.com>
2022-11-04 23:51:30 +0200

    database

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask
-from db import getDB
-import json
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String(32),  unique=True, nullable=False)
+    
+    @property
+    def serialize(self):
+       """Return object data in easily serializable format"""
+       return {
+           'id'         : self.id,
+           'uuid': self.uuid,
+       }
+
+with app.app_context():
+    db.create_all()
+
+    # db.session.add(User(uuid="example"))
+    # db.session.commit()
+
+    
 
 @app.route("/")
 def hello_world():
-    with getDB(app.config) as db:
-        cursor = db.cursor()
-        cursor.execute("SHOW DATABASES")
-        databases = cursor.fetchall()
-        return "test"
+    query = db.session.query(User).order_by(User.id)
+    return jsonify(json_list = [i.serialize for i in query.all()])
