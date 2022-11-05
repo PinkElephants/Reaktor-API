5cf53aadbbc4d2cb88539662a57b5c3487271765
Egor Grishechko <egorgrishechko@gmail.com>
2022-11-05 12:41:15 +0200

    Fixing migrations

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app.config.from_pyfile("config.py")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     uuid = db.Column(db.String(32),  unique=True, nullable=False)
