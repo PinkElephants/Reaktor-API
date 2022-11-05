9d916a3fdcf21885e69aff19f9d1219a15a243ad
Egor Grishechko <egorgrishechko@gmail.com>
2022-11-05 08:26:43 +0200

    database migrations and models

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     uuid = db.Column(db.String(32),  unique=True, nullable=False)
+    startJob = db.Column(db.DateTime, nullable=True)
+    finishJob = db.Column(db.DateTime, nullable=True)
     
     @property
     def serialize(self):
@@ -28,5 +32,11 @@ with app.app_context():
 
 @app.route("/")
 def hello_world():
+    return jsonify(status = "I am working")
+
+
+
+@app.route("/user")
+def user_profile():
     query = db.session.query(User).order_by(User.id)
     return jsonify(json_list = [i.serialize for i in query.all()])
