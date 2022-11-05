99385b2b19870ec6133c1bbbf97f6ade6b1f9df3
Egor Grishechko <egorgrishechko@gmail.com>
2022-11-05 21:04:08 +0200

    Music mood

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, abort, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import datetime
+import json
 
 
 app = Flask(__name__)
@@ -26,9 +27,88 @@ class User(db.Model):
            'finish_job' : self.finishJob,           
        }
 
+class MusicMood(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_uuid = db.Column(db.String(90), nullable=False)
+    played_at = db.Column(db.DateTime, nullable=False)
+    major_sum = db.Column(db.Float, nullable=False)
+    minor_sum = db.Column(db.Float, nullable=False)
+    unconf_sum = db.Column(db.Float, nullable=False)
+    happyness_index = db.Column(db.Float, nullable=False)
+    sadness_indes = db.Column(db.Float, nullable=False)
+
+
+    def serialize(self, user_uuid):
+       """Return object data in easily serializable format"""
+       return {
+           'id'         : self.id,
+           'user_uuid'  : user_uuid,
+           'played_at'  : self.played_at,
+           'major_sum'  : self.major_sum,
+           'minor_sum'  : self.minor_sum,     
+           'unconf_sum'  : self.unconf_sum,
+           'happyness_index'  : self.happyness_index,    
+           'sadness_indes'  : self.sadness_indes,       
+       }
+
+
 with app.app_context():
     db.create_all()
+    db.session.query(MusicMood).delete()
+    db.session.commit()
     
+    db.session.add(MusicMood(
+        id = 1,
+        user_uuid = "861B760D-EE8A-4906-BAF3-DCEBA08C6243",
+        played_at = datetime.datetime.fromtimestamp(1667260800),
+        major_sum = 0,
+        minor_sum = 3,
+        unconf_sum = 1,
+        happyness_index = 0.0,
+        sadness_indes = 0.0,
+    ))
+    db.session.add(MusicMood(
+        id = 2,
+        user_uuid = "861B760D-EE8A-4906-BAF3-DCEBA08C6243",
+        played_at = datetime.datetime.fromtimestamp(1667347200),
+        major_sum = 2,
+        minor_sum = 0,
+        unconf_sum = 0,
+        happyness_index = 1,
+        sadness_indes = 1,
+    ))
+    db.session.add(MusicMood(
+        id = 3,
+        user_uuid = "861B760D-EE8A-4906-BAF3-DCEBA08C6243",
+        played_at = datetime.datetime.fromtimestamp(1667433600),
+        major_sum = 12,
+        minor_sum = 12,
+        unconf_sum = 3,
+        happyness_index = 0.4444444444,
+        sadness_indes = 0.4444444444,
+    ))
+    db.session.add(MusicMood(
+        id = 4,
+        user_uuid = "861B760D-EE8A-4906-BAF3-DCEBA08C6243",
+        played_at = datetime.datetime.fromtimestamp(1667520000),
+        major_sum = 1,
+        minor_sum = 1,
+        unconf_sum = 0,
+        happyness_index = 0.5,
+        sadness_indes = 0.5,
+    ))
+    db.session.add(MusicMood(
+        id = 5,
+        user_uuid = "861B760D-EE8A-4906-BAF3-DCEBA08C6243",
+        played_at = datetime.datetime.fromtimestamp(1667606400),
+        major_sum = 5,
+        minor_sum = 8,
+        unconf_sum = 2,
+        happyness_index = 0.3333333333,
+        sadness_indes = 0.3333333333,
+    ))
+    db.session.commit()
+
 @app.route("/")
 def hello_world():
     return jsonify(status = "I am working")
@@ -66,4 +146,7 @@ def user_profile(uuid_request):
     if user is None:
         abort(Response("User not found", 404))
 
-    return jsonify(user.serialize)
+    music = db.session.query(MusicMood).all()
+    userData = user.serialize
+    userData["music_mood"] = [mm.serialize(uuid_request) for mm in music]
+    return jsonify(userData)
---
+++ spotify_songs/google_parsing/music.py
@@ -0,0 +1,39 @@
+import numpy as np
+import json
+import pandas as pd
+import datetime
+from dateutil.parser import parse
+
+
+
+def process_music(json):
+    data = pd.DataFrame([ {"played_at":parse(item['played_at']).date(),
+                           "mode": item['track_statistic']['mode'],
+                           "mode_confidence": item['track_statistic']['mode_confidence'],
+                           "confident_major": item['track_statistic']['mode'] == 1 and item['track_statistic']['mode_confidence'] > 0.4,
+                           "confident_minor": item['track_statistic']['mode'] == 0 and item['track_statistic']['mode_confidence'] > 0.4,
+                           "unconfident": item['track_statistic']['mode_confidence'] < 0.4
+                           } for item in json_file])
+
+
+    confident_major = data.groupby(['played_at'])['confident_major'].sum().reset_index(name='sum')
+
+    confident_minor = data.groupby(['played_at'])['confident_minor'].sum().reset_index(name='sum')
+
+    unconfident = data.groupby(['played_at'])['unconfident'].sum().reset_index(name='sum')
+
+    confident_major= confident_major.rename(columns={'sum':'major_sum'})
+    confident_minor= confident_minor.rename(columns={'sum':'minor_sum'})
+    unconfident= unconfident.rename(columns={'sum':'unconf_sum'})
+
+    res = pd.concat([confident_major,confident_minor,unconfident], axis=1)
+    res = res.loc[:,~res.columns.duplicated()]
+    res['happyness_index'] = res['major_sum'] / (res['major_sum'] + res['minor_sum'] +res['unconf_sum'] + np.finfo(float).eps)
+    res['sadness_indes'] = res['major_sum'] / (res['major_sum'] + res['minor_sum'] +res['unconf_sum'] + np.finfo(float).eps)
+    return res
+
+with open('parsed_music.json') as f:
+    json_file = json.load(f)
+
+res = process_music(json)
+print(res.to_json())
